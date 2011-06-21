76675498425505d663979eb1bc10423da471421b
Felipe Ribeiro <felipernb@gmail.com>
2011-06-21 10:42:53 +0200

    moving is_pandigital to a separated file

--- euler_41.rb
+++ euler_41.rb
@@ -1,14 +1,6 @@
-def is_pandigital(n)
-	n = n.to_s if n.class != String
-	digits = n.split(//).map{|i| i.to_i}.sort
-	return false if digits.length != digits.max
-	(1..digits.max).each do |i|
-		return false if digits[i-1] != i 
-	end
-	return true
-end
-
 require 'eratosthenes'
+require 'pandigital'
+
 # there can be no pandigital prime with 8 or 9 digits, because the sum of all digits, in both cases, is multiple of 9
 Eratosthenes.calculate_primes(7654321)
 7654321.downto(1) do |i|
