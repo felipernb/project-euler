7927588d35ba4f5ada2b45ee7336ff615f21a3a9
Felipe Ribeiro <felipernb@gmail.com>
2011-06-21 10:41:26 +0200

    changing 'if not' to 'unless'

--- pandigital.rb
+++ pandigital.rb
@@ -1,5 +1,5 @@
 def is_pandigital(n)
-	n = n.to_s if n.class != String
+	n = n.to_s unless n.class == String
 	digits = n.split(//).map{|i| i.to_i}.sort
 	return false unless digits.length == digits.max
 	(1..digits.max).each do |i|
